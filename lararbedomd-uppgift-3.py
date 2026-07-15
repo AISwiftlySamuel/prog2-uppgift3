b2a076e2f695b414bd8af29ec001aaf72ea3614f
AISwiftlySamuel <samuel.augsburger@aiswiftly.com>
2026-07-15 20:07:48 +0200

    Create lararbedomd-uppgift-3.py

---
+++ lararbedomd-uppgift-3.py
@@ -0,0 +1,5 @@
+class Tjanst:
+    def __init__(self, namn, pris_per_manad, konsultationstimmar):
+        self.namn = namn
+        self.pris_per_manad = pris_per_manad
+        self.konsultationstimmar = konsultationstimmar
